e62ef2c5806fbd20bd5484af02a59d7597b34dd3
running <A576236148946@126.com>
2020-11-13 13:41:38 +0800

    by cxl

---
+++ app/src/main/java/com/grayvtouch/xinqu/App.java
@@ -0,0 +1,14 @@
+package com.grayvtouch.xinqu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class App extends AppCompatActivity
+{
+    public void onCreate(Bundle bundle)
+    {
+        super.onCreate(bundle);
+        this.setContentView(R.layout.app);
+    }
+}
